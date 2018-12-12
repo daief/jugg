e5b5a0e8c44e8442e71cd15f80b0452c84548a4c
daief <1437931235@qq.com>
2018-12-12 20:13:30 +0800

    refactor(multi html): rename react to multi html

---
+++ packages/multi-html/src/index.ts
@@ -0,0 +1,52 @@
+import { Plugin, Jugg } from '@jugg/jugg';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import nodePath from 'path';
+import fs from 'fs';
+
+export interface Option {
+  routes?: Array<{
+    path: string;
+    template?: string;
+  }>;
+}
+
+const p: Plugin = (jugg: Jugg, opts: Option) => {
+  const { routes } = opts;
+  const { IsProd, Utils } = jugg;
+  const { getAbsolutePath } = Utils;
+
+  return ({ config }) => {
+    if (IsProd) {
+      (routes || []).forEach((route, index) => {
+        const { path, template = '' } = route;
+
+        // root path
+        if (path === '/') {
+          return;
+        }
+
+        const tpl = nodePath.join(process.cwd(), 'src', template);
+        const defaultTpl = nodePath.join(process.cwd(), 'src', 'document.ejs');
+        const builtInTpl = nodePath.join(__dirname, 'document.ejs');
+
+        config
+          .plugin(`html-webpack-plugin-multi-html-${index}`)
+          .use(HtmlWebpackPlugin, [
+            {
+              filename: getAbsolutePath('dist', path, 'index.html'),
+              template:
+                template && fs.existsSync(tpl)
+                  ? tpl
+                  : fs.existsSync(defaultTpl)
+                  ? defaultTpl
+                  : builtInTpl,
+              inject: true,
+            },
+          ])
+          .end();
+      });
+    }
+  };
+};
+
+export default p;
