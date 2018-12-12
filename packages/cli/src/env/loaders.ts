30370c3331ffe3950d3dfa355ae271a1450b09f0
daief <1437931235@qq.com>
2018-12-12 13:42:40 +0800

    feat(cli): css module,exclude node_modules & feat width .module.(le|c)ss

--- packages/cli/src/env/loaders.ts
+++ packages/cli/src/env/loaders.ts
@@ -1,4 +1,5 @@
 import Config from 'webpack-chain';
+// import { getAbsolutePath } from '../utils';
 
 export default (config: Config) => {
   const isProd = process.env.NODE_ENV === 'production';
@@ -58,6 +59,18 @@ export default (config: Config) => {
 };
 
 function setStyleLoaders(config: Config) {
+  function cssExclude(filePath: string) {
+    if (/node_modules/.test(filePath)) {
+      return true;
+    }
+
+    if (/\.module\.(css|less|sass|scss)$/.test(filePath)) {
+      return true;
+    }
+
+    return false;
+  }
+
   // TODO refine here & else about
   const isProd = process.env.NODE_ENV === 'production';
 
@@ -67,39 +80,89 @@ function setStyleLoaders(config: Config) {
   };
 
   // --------------- support css & less
-  const cssRule = config.module.rule('css').test(/\.(le|c)ss$/i);
-  if (!isProd) {
-    cssRule.use('css-hot-loader').loader(require.resolve('css-hot-loader'));
+  function setCssLoaders(
+    rule: Config.Rule,
+    opts: {
+      isModule?: boolean;
+      less?: boolean;
+    }
+  ) {
+    const defaultOpts = {
+      isModule: true,
+    };
+    const { isModule, less } = { ...defaultOpts, ...opts };
+
+    if (!isProd) {
+      rule.use('css-hot-loader').loader(require.resolve('css-hot-loader'));
+    }
+
+    // notice the order
+    // remember to add plugin
+    rule.use('extract-css-loader').loader(require('mini-css-extract-plugin').loader);
+
+    rule
+      .use('css-loader')
+      .loader(require.resolve('css-loader'))
+      .options({
+        ...(isModule ? cssModulesConfig : {}),
+      });
+
+    rule
+      .use('postcss-loader')
+      .loader(require.resolve('postcss-loader'))
+      .options({
+        plugins: () => [
+          require('postcss-flexbugs-fixes'),
+          require('autoprefixer')({
+            browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+            flexbox: 'no-2009',
+          }),
+        ],
+      });
+
+    if (less === true) {
+      rule
+        .use('less-loader')
+        .loader(require.resolve('less-loader'))
+        .options({
+          javascriptEnabled: true,
+        });
+    }
   }
 
-  // remember to add plugin
-  cssRule.use('extract-css-loader').loader(require('mini-css-extract-plugin').loader);
+  setCssLoaders(
+    config.module
+      .rule('css.module')
+      .test(/\.module\.(le|c)ss$/i)
+      .exclude.add(path => /node_modules/i.test(path))
+      .end(),
+    {
+      isModule: true,
+      less: true,
+    }
+  );
 
-  cssRule
-    .use('css-loader')
-    .loader(require.resolve('css-loader'))
-    .options({
-      ...cssModulesConfig,
-    });
+  setCssLoaders(
+    config.module
+      .rule('css')
+      .test(/\.(le|c)ss$/i)
+      .exclude.add(cssExclude)
+      .end(),
+    {
+      isModule: false,
+      less: true,
+    }
+  );
 
-  cssRule
-    .use('postcss-loader')
-    .loader(require.resolve('postcss-loader'))
-    .options({
-      plugins: () => [
-        require('postcss-flexbugs-fixes'),
-        require('autoprefixer')({
-          browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
-          flexbox: 'no-2009',
-        }),
-      ],
-    });
-
-  cssRule
-    .use('less-loader')
-    .loader(require.resolve('less-loader'))
-    .options({
-      javascriptEnabled: true,
-      ...cssModulesConfig,
-    });
+  setCssLoaders(
+    config.module
+      .rule('css-in-node_modules')
+      .test(/\.(le|c)ss$/i)
+      .include.add(/node_modules/)
+      .end(),
+    {
+      isModule: false,
+      less: true,
+    }
+  );
 }
