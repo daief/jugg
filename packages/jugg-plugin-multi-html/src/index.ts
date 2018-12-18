97e2a35135345a99cabb0a99aec85c5691fb575b
daief <1437931235@qq.com>
2018-12-18 20:14:21 +0800

    refactor(html plugin): refactor:

--- packages/jugg-plugin-multi-html/src/index.ts
+++ packages/jugg-plugin-multi-html/src/index.ts
@@ -1,4 +1,4 @@
-import { Plugin, Jugg } from '@axew/jugg';
+import { Plugin } from '@axew/jugg';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import nodePath from 'path';
 import fs from 'fs';
@@ -10,55 +10,58 @@ export interface Option {
   }>;
 }
 
-const p: Plugin = (jugg: Jugg, opts: Option) => {
+const p: Plugin = (api, opts: Option) => {
   const { routes } = opts;
-  const { IsProd, Utils } = jugg;
-  const { getAbsolutePath } = Utils;
 
-  return ({ config }) => {
-    if (IsProd) {
-      (routes || []).forEach((route, index) => {
-        const { path, template = '' } = route;
+  api.chainWebpack(({ config }) => {
+    const { IsProd, Utils } = api.jugg;
+    const { getAbsolutePath } = Utils;
 
-        // root path
-        if (path === '/') {
-          return;
-        }
-
-        const tpl = nodePath.join(process.cwd(), 'src', template);
-        const defaultTpl = nodePath.join(process.cwd(), 'src', 'document.ejs');
-        const builtInTpl = nodePath.join(__dirname, 'document.ejs');
-
-        config
-          .plugin(`html-webpack-plugin-multi-html-${index}`)
-          .use(HtmlWebpackPlugin, [
-            {
-              filename: getAbsolutePath('dist', path, 'index.html'),
-              template:
-                template && fs.existsSync(tpl)
-                  ? tpl
-                  : fs.existsSync(defaultTpl)
-                  ? defaultTpl
-                  : builtInTpl,
-              inject: true,
-              minify: {
-                caseSensitive: true,
-                collapseWhitespace: true,
-                conservativeCollapse: true,
-                removeAttributeQuotes: false,
-                removeComments: true,
-                removeScriptTypeAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                minifyCSS: true,
-                minifyJS: true,
-                minifyURLs: true,
-              },
-            },
-          ])
-          .end();
-      });
+    if (!IsProd) {
+      return;
     }
-  };
+
+    (routes || []).forEach((route, index) => {
+      const { path, template = '' } = route;
+
+      // root path
+      if (path === '/') {
+        return;
+      }
+
+      const tpl = nodePath.join(process.cwd(), 'src', template);
+      const defaultTpl = nodePath.join(process.cwd(), 'src', 'document.ejs');
+      const builtInTpl = nodePath.join(__dirname, 'document.ejs');
+
+      config
+        .plugin(`html-webpack-plugin-multi-html-${index}`)
+        .use(HtmlWebpackPlugin, [
+          {
+            filename: getAbsolutePath('dist', path, 'index.html'),
+            template:
+              template && fs.existsSync(tpl)
+                ? tpl
+                : fs.existsSync(defaultTpl)
+                ? defaultTpl
+                : builtInTpl,
+            inject: true,
+            minify: {
+              caseSensitive: true,
+              collapseWhitespace: true,
+              conservativeCollapse: true,
+              removeAttributeQuotes: false,
+              removeComments: true,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              minifyCSS: true,
+              minifyJS: true,
+              minifyURLs: true,
+            },
+          },
+        ])
+        .end();
+    });
+  });
 };
 
 export default p;
