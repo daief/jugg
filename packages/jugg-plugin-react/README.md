44e136e6a055484edcd4edf88b36db93b3784f73
daief <1437931235@qq.com>
2018-12-29 15:48:17 +0800

    feat(plugin react): support react hot reload

--- packages/jugg-plugin-react/README.md
+++ packages/jugg-plugin-react/README.md
@@ -17,3 +17,18 @@ interface JReactPresetOption {
 }
 
 ```
+
+# react hot loader
+
+[you should add code like this](https://www.npmjs.com/package/react-hot-loader/v/4.6.3#getting-started):
+
+```js
+// App.js
+import { hot } from 'react-hot-loader/root'
+const App = () => <div>Hello World!</div>
+export default hot(App)
+```
+
+## notice
+
+- Want to support hot reload friendly in TypeScript, you'd better change ts compiler to babel, set `compileTs: true` in `jugg-plugin-babel` config, [detail](https://www.npmjs.com/package/react-hot-loader/v/4.6.3#typescript).
--- packages/jugg-plugin-react/src/index.ts
+++ packages/jugg-plugin-react/src/index.ts
@@ -12,21 +12,34 @@ export default (api: PluginAPI, opts: Options) => {
   const { jReactPresetOption } = opts;
 
   api.chainWebpack(({ config }) => {
-    if (config.module.rules.has('jugg-plugin-babel-rule')) {
+    const JS_BABEL = 'jugg-plugin-babel-rule';
+    const TS_BABEL = 'jugg-plugin-babel-ts-rule';
+
+    if (config.module.rules.has(JS_BABEL)) {
+      const tapOptions = (c: any = {}) => {
+        const { presets, plugins } = c;
+        return {
+          ...c,
+          presets: [[require.resolve('./preset'), jReactPresetOption], ...(presets || [])],
+          plugins: [
+            ...(plugins || []),
+            // XXX works only set here, if set in `./preset`, it does not work
+            require.resolve('react-hot-loader/babel'),
+          ],
+        };
+      };
+
       config.module
-        .rule('jugg-plugin-babel-rule')
-        // ---------------------------- works with jugg-plugin-babel
-        // .test(/\.jsx?$/)
-        // .exclude.add(filepath => /node_modules/.test(filepath))
-        // .end()
+        .rule(JS_BABEL)
         .use('babel-loader')
-        .tap(c => {
-          const { presets } = c;
-          return {
-            ...c,
-            presets: [...(presets || []), [require.resolve('./preset'), jReactPresetOption]],
-          };
-        });
+        .tap(tapOptions);
+
+      if (config.module.rules.has(TS_BABEL)) {
+        config.module
+          .rule(TS_BABEL)
+          .use('babel-loader')
+          .tap(tapOptions);
+      }
     } else {
       api.jugg.Utils.logger.error('`jugg-plugin-react` works with `jugg-plugin-babel`');
     }
