a7949fe8022419ef6b5b1eeaef471e2476290dc3
daief <1437931235@qq.com>
2019-01-24 10:53:09 +0800

    chore(jugg-plugin-react): 添加测试用例

--- packages/jugg-plugin-react/test/ts-rhl-transformer.dev.test.ts
+++ packages/jugg-plugin-react/test/ts-rhl-transformer.dev.test.ts
@@ -5,19 +5,40 @@ import { readdirSync, statSync, readFileSync } from 'fs';
 const printer = ts.createPrinter();
 const FIXTURES_DIR = join(__dirname, '__fixtures__');
 
-describe('test', () => {
-  readdirSync(FIXTURES_DIR).forEach(fixtureName => {
-    const fixtureFile = join(FIXTURES_DIR, fixtureName);
+describe('ts-rhl-transformer', () => {
+  describe('react components', () => {
+    readdirSync(FIXTURES_DIR).forEach(fixtureName => {
+      const fixtureFile = join(FIXTURES_DIR, fixtureName);
 
-    if (statSync(fixtureFile).isFile()) {
-      it(fixtureName, () => {
-        const sourceCode = readFileSync(fixtureFile, 'utf-8');
-        const source = ts.createSourceFile(fixtureName, sourceCode, ts.ScriptTarget.ES2016, true);
-        const result = ts.transform(source, [transformerFactory()]);
-        const transformedSourceFile = result.transformed[0];
-        const resultCode = printer.printFile(transformedSourceFile);
-        expect(resultCode).toMatchSnapshot();
-      });
-    }
+      if (statSync(fixtureFile).isFile()) {
+        it(fixtureName, () => {
+          const sourceCode = readFileSync(fixtureFile, 'utf-8');
+          const source = ts.createSourceFile(fixtureName, sourceCode, ts.ScriptTarget.ES2016, true);
+          const result = ts.transform(source, [transformerFactory()]);
+          const transformedSourceFile = result.transformed[0];
+          const resultCode = printer.printFile(transformedSourceFile);
+          expect(resultCode).toMatchSnapshot();
+        });
+      }
+    });
+  });
+
+  describe('copies arrow function body block onto hidden class methods', () => {
+    const fixturesDir = join(FIXTURES_DIR, 'class-properties');
+
+    readdirSync(fixturesDir).forEach(fixtureName => {
+      const fixtureFile = join(fixturesDir, fixtureName);
+
+      if (statSync(fixtureFile).isFile()) {
+        it(fixtureName, () => {
+          const sourceCode = readFileSync(fixtureFile, 'utf-8');
+          const source = ts.createSourceFile(fixtureName, sourceCode, ts.ScriptTarget.ES2016, true);
+          const result = ts.transform(source, [transformerFactory()]);
+          const transformedSourceFile = result.transformed[0];
+          const resultCode = printer.printFile(transformedSourceFile);
+          expect(resultCode).toMatchSnapshot();
+        });
+      }
+    });
   });
 });
