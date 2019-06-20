e257648e2864469be32e3f0b7a0075b49383a3a3
daief <1437931235@qq.com>
2019-06-20 15:47:59 +0800

    refactor(ts-lib): refine package build

--- examples/ts-lib/.juggrc.ts
+++ examples/ts-lib/.juggrc.ts
@@ -1,23 +1,40 @@
 import { extendConfig } from '@axew/jugg';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default extendConfig({
   plugins: ['@axew/jugg-plugin-vue', '@axew/jugg-plugin-lib'],
   tsCustomTransformers: {
     before: ['@axew/jugg-plugin-react/lib/ts-rhl-transformer'],
   },
   webpack({ config }) {
+    if (!isProd) {
+      config
+        .entry('dev')
+        .add('./dev/index')
+        .end();
+    } else {
+      config.output
+        .library('TestLibrary')
+        .libraryTarget('umd')
+        .end()
+        .externals({
+          vue: 'vue',
+          react: 'react',
+        })
+        .end();
+    }
+
     config
-      .entry('dev')
-      .add('./dev/index')
-      .end()
-      .entry('style')
+      .entry('index')
       .add('./src/style')
       .end();
   },
-  html:
-    process.env.NODE_ENV === 'production'
-      ? false
-      : {
-          template: 'dev/document.ejs',
-        },
+  html: isProd
+    ? false
+    : {
+        template: 'dev/document.ejs',
+      },
+  filename: 'index',
+  chunks: false,
 });
