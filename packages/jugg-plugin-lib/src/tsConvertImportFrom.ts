e8e6941965fc77db58558c80e980cffe72ce9393
daief <1437931235@qq.com>
2019-10-23 18:04:29 +0800

    fix(jugg-plugin-lib): check to use a new node or old node

--- packages/jugg-plugin-lib/src/tsConvertImportFrom.ts
+++ packages/jugg-plugin-lib/src/tsConvertImportFrom.ts
@@ -24,14 +24,25 @@ export interface IOptions {
 export function createTransformer(opts: IOptions = {}) {
   const { match = /\.vue$/i, target = '.js' } = opts;
 
+  // TODO 生成的类型文件中的导出没能随着更新
   const createNewStringLiteralByOpts = (node: ts.StringLiteral) => {
-    return ts.createStringLiteral(node.text.replace(match, target));
+    const newText = node.text.replace(match, target);
+    /**
+     * XXX 未变化时，返回原节点，新增的节点会有如下影响
+     * ```ts
+     *  import { functionA, InterfaceA } from 'some.vue'
+     *  // ! 在结果文件 js 中，类型 `InterfaceA` 应该被删除，但实际上会被保留
+     *  import { functionA, InterfaceA } from 'some.js'
+     * ```
+     */
+    return newText !== node.text ? ts.createStringLiteral(newText) : node;
   };
 
   const transformer: ts.TransformerFactory<ts.SourceFile> = context => {
     return transformerNode => {
       const hasDisableComment = (node: ts.Node) => {
-        const range = ts.getTrailingCommentRanges(transformerNode.text, node.end) || [];
+        const range =
+          ts.getTrailingCommentRanges(transformerNode.text, node.end) || [];
 
         return (
           range
@@ -39,7 +50,8 @@ export function createTransformer(opts: IOptions = {}) {
               return transformerNode.text.substring(ran.pos, ran.end);
             })
             // use `jugg-lib-disable` to disable converting
-            .filter(comment => /\/\/\s*jugg-lib-disable/i.test(comment)).length > 0
+            .filter(comment => /\/\/\s*jugg-lib-disable/i.test(comment))
+            .length > 0
         );
       };
 
@@ -56,21 +68,24 @@ export function createTransformer(opts: IOptions = {}) {
                 node.decorators,
                 node.modifiers,
                 node.importClause,
-                newImportFrom
+                newImportFrom,
               )
             : ts.updateExportDeclaration(
                 node,
                 node.decorators,
                 node.modifiers,
                 node.exportClause,
-                newImportFrom
+                newImportFrom,
               );
         }
         return ts.visitEachChild(node, visitorImport, context);
       };
 
       const visitRequire: ts.Visitor = node => {
-        if (ts.isExpressionStatement(node) && ts.isCallExpression(node.expression)) {
+        if (
+          ts.isExpressionStatement(node) &&
+          ts.isCallExpression(node.expression)
+        ) {
           /**
            * make is `require('..')`
            */
@@ -90,13 +105,18 @@ export function createTransformer(opts: IOptions = {}) {
 
           return ts.updateExpressionStatement(
             node,
-            ts.createIdentifier(`require("${createNewStringLiteralByOpts(requireStr).text}")`)
+            ts.createIdentifier(
+              `require("${createNewStringLiteralByOpts(requireStr).text}")`,
+            ),
           );
         }
         return ts.visitEachChild(node, visitRequire, context);
       };
 
-      return ts.visitNode(ts.visitNode(transformerNode, visitorImport), visitRequire);
+      return ts.visitNode(
+        ts.visitNode(transformerNode, visitorImport),
+        visitRequire,
+      );
     };
   };
   return transformer;
