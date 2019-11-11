8a1b0df2dc4336741ebf2b90ab607f3b1b08fcbc
daief <1437931235@qq.com>
2019-11-11 16:46:39 +0800

    refactor(jugg-plugin-lib): 更改编译模块的行为方式，以迎合 tsCustomTransformers 的使用

--- packages/jugg-plugin-lib/src/gulpfile.ts
+++ packages/jugg-plugin-lib/src/gulpfile.ts
@@ -107,8 +107,7 @@ export default (opts: IOptions, api: PluginAPI) => {
             })
             .catch(errors.push);
         }),
-      )
-      .pipe(gulp.dest(TARGET_DIR));
+      );
 
     // copy files
     const assets = gulp
@@ -119,80 +118,51 @@ export default (opts: IOptions, api: PluginAPI) => {
           })`,
         ),
       )
-      .pipe(filterTest())
-      .pipe(gulp.dest(TARGET_DIR));
+      .pipe(filterTest());
 
-    const compileTS = (src?: string[], tsOpts: any = {}) => {
-      // 读取配置
-      const { before = [], after = [] } = tsCustomTransformers
-        ? tsCustomTransformers({ isEsModule })
-        : api.jugg.JConfig.tsCustomTransformers!;
+    // const compileTS = (src?: string[], tsOpts: any = {}) => {
+    //   // 读取配置
+    //   const { before = [], after = [] } = tsCustomTransformers
+    //     ? tsCustomTransformers({ isEsModule })
+    //     : api.jugg.JConfig.tsCustomTransformers!;
 
-      const TS_CONFIG_FILE = getAbsolutePath(
-        process.env.JUGG_TS_PROJECT || 'tsconfig.json',
-      );
+    //   const TS_CONFIG_FILE = getAbsolutePath(
+    //     process.env.JUGG_TS_PROJECT || 'tsconfig.json',
+    //   );
 
-      const tsProject = gulpTs.createProject(TS_CONFIG_FILE, {
-        noUnusedParameters: true,
-        noUnusedLocals: true,
-        strictNullChecks: true,
-        allowSyntheticDefaultImports: true,
-        target: 'es5',
-        moduleResolution: 'node',
-        module: isEsModule ? 'esnext' : 'commonjs',
-        ...tsOpts,
-        getCustomTransformers: () => ({
-          before: [transformerFactory(), ...before.map(resolvePlugin)],
-          after: after.map(resolvePlugin),
-        }),
-      });
+    //   const tsProject = gulpTs.createProject(TS_CONFIG_FILE, {
+    //     noUnusedParameters: true,
+    //     noUnusedLocals: true,
+    //     strictNullChecks: true,
+    //     allowSyntheticDefaultImports: true,
+    //     target: 'es5',
+    //     moduleResolution: 'node',
+    //     module: isEsModule ? 'esnext' : 'commonjs',
+    //     ...tsOpts,
+    //     getCustomTransformers: () => ({
+    //       before: [transformerFactory(), ...before.map(resolvePlugin)],
+    //       after: after.map(resolvePlugin),
+    //     }),
+    //   });
 
-      if (src) {
-        const rs = gulp
-          .src(src)
-          .pipe(filterTest())
-          .pipe(tsProject(gulpTs.reporter.fullReporter(true)))
-          // https://github.com/ivogabe/gulp-typescript/issues/295
-          .on('error', errors.push);
+    //   if (src) {
+    //     const rs = gulp
+    //       .src(src)
+    //       .pipe(filterTest())
+    //       .pipe(tsProject(gulpTs.reporter.fullReporter(true)))
+    //       // https://github.com/ivogabe/gulp-typescript/issues/295
+    //       .on('error', errors.push);
 
-        return rs;
-      }
+    //     return rs;
+    //   }
 
-      return through2
-        .obj((file, _, next) => {
-          next(null, file);
-        })
-        .pipe(tsProject(gulpTs.reporter.fullReporter(true)))
-        .on('error', errors.push);
-    };
-
-    // use tsc compile ts, tsx, with declaration files
-    const tsResult = compileTS(getSourceFilesArray('@(ts|tsx)'), {
-      allowJs: false,
-      declaration: true,
-    });
-
-    // use tsc compile js, jsx, no declaration files
-    const tsJsResult = compileTS(getSourceFilesArray('@(js|jsx)'), {
-      allowJs: true,
-      declaration: false,
-      // isolatedModules (boolean) - Compiles files seperately and doesn't check types,
-      // which causes a big speed increase. You have to use gulp-plumber and TypeScript 1.5+.
-      isolatedModules: true,
-    });
-
-    const vueResult = gulp
-      .src(getSourceFilesArray('vue'))
-      .pipe(filterTest())
-      .pipe(gulpVue(api))
-      .pipe(
-        compileTS(void 0, {
-          allowJs: true,
-          declaration: false,
-          isolatedModules: true,
-        }),
-      )
-      .js.pipe(gulp.dest(TARGET_DIR));
+    //   return through2
+    //     .obj((file, _, next) => {
+    //       next(null, file);
+    //     })
+    //     .pipe(tsProject(gulpTs.reporter.fullReporter(true)))
+    //     .on('error', errors.push);
+    // };
 
     const convertLessImport2CssStream = () =>
       through2.obj(function z(file, encoding, next) {
@@ -217,18 +187,190 @@ export default (opts: IOptions, api: PluginAPI) => {
         }
       });
 
+    const compileTSPipe = (
+      tsOpts: any = {},
+      /**
+       * 是否使用 transformer
+       */
+      withTransformer = true,
+    ) => {
+      // 读取配置
+      const { before = [], after = [] } = tsCustomTransformers
+        ? tsCustomTransformers({ isEsModule })
+        : api.jugg.JConfig.tsCustomTransformers!;
+
+      const TS_CONFIG_FILE = getAbsolutePath(
+        process.env.JUGG_TS_PROJECT || 'tsconfig.json',
+      );
+
+      const tsProject = gulpTs.createProject(TS_CONFIG_FILE, {
+        noUnusedParameters: true,
+        noUnusedLocals: true,
+        strictNullChecks: true,
+        allowSyntheticDefaultImports: true,
+        target: 'es5',
+        moduleResolution: 'node',
+        module: isEsModule ? 'esnext' : 'commonjs',
+        ...tsOpts,
+        getCustomTransformers: () =>
+          withTransformer
+            ? {
+                before: [transformerFactory(), ...before.map(resolvePlugin)],
+                after: after.map(resolvePlugin),
+              }
+            : {
+                before: [],
+                after: [],
+              },
+      });
+
+      return through2
+        .obj((file, _, next) => {
+          next(null, file);
+        })
+        .pipe(tsProject(gulpTs.reporter.fullReporter(true)))
+        .on('error', errors.push);
+    };
+
+    let codeSourcePipeline: any;
+    if (isEsModule) {
+      // 处理 es 情况下编译
+      const tsResult = gulp.src(getSourceFilesArray('@(ts|tsx)')).pipe(
+        compileTSPipe({
+          allowJs: false,
+          declaration: true,
+        }),
+      );
+      const jsResult = gulp.src(getSourceFilesArray('@(js|jsx)')).pipe(
+        compileTSPipe({
+          allowJs: true,
+          declaration: false,
+        }),
+      );
+      const vueResult = gulp
+        .src(getSourceFilesArray('vue'))
+        .pipe(filterTest())
+        .pipe(gulpVue(api))
+        .pipe(
+          compileTSPipe({
+            allowJs: true,
+            declaration: false,
+            isolatedModules: true,
+          }),
+        );
+      codeSourcePipeline = merge2([
+        tsResult.js.pipe(convertLessImport2CssStream()),
+        tsResult.dts,
+        jsResult.js.pipe(convertLessImport2CssStream()),
+        vueResult.js,
+      ]);
+    } else {
+      // lib 下，先使用 es module 编译，不然 ts-import-plugin 会有问题
+      // 再将 es module => commonjs
+      const tsResult = gulp.src(getSourceFilesArray('@(ts|tsx)')).pipe(
+        compileTSPipe({
+          allowJs: false,
+          declaration: true,
+          module: 'esnext',
+        }),
+      );
+      const tsJsResult = tsResult.js.pipe(
+        compileTSPipe(
+          {
+            allowJs: true,
+            declaration: false,
+          },
+          false,
+        ),
+      );
+      const jsResult = gulp
+        .src(getSourceFilesArray('@(js|jsx)'))
+        .pipe(
+          compileTSPipe({
+            allowJs: true,
+            declaration: false,
+            module: 'esnext',
+          }),
+        )
+        .js.pipe(
+          compileTSPipe(
+            {
+              allowJs: true,
+              declaration: false,
+            },
+            false,
+          ),
+        );
+      const vueResult = gulp
+        .src(getSourceFilesArray('vue'))
+        .pipe(filterTest())
+        .pipe(gulpVue(api))
+        .pipe(
+          compileTSPipe({
+            allowJs: true,
+            declaration: false,
+            isolatedModules: true,
+            module: 'esnext',
+          }),
+        )
+        .js.pipe(
+          compileTSPipe(
+            {
+              allowJs: true,
+              declaration: false,
+            },
+            false,
+          ),
+        );
+      codeSourcePipeline = merge2([
+        tsJsResult.js.pipe(convertLessImport2CssStream()),
+        tsResult.dts,
+        jsResult.js.pipe(convertLessImport2CssStream()),
+        vueResult.js,
+      ]);
+    }
+
+    // // use tsc compile ts, tsx, with declaration files
+    // const tsResult = compileTS(getSourceFilesArray('@(ts|tsx)'), {
+    //   allowJs: false,
+    //   declaration: true,
+    // });
+
+    // // use tsc compile js, jsx, no declaration files
+    // const tsJsResult = compileTS(getSourceFilesArray('@(js|jsx)'), {
+    //   allowJs: true,
+    //   declaration: false,
+    //   // isolatedModules (boolean) - Compiles files seperately and doesn't check types,
+    //   // which causes a big speed increase. You have to use gulp-plumber and TypeScript 1.5+.
+    //   isolatedModules: true,
+    // });
+
+    // const vueResult = gulp
+    //   .src(getSourceFilesArray('vue'))
+    //   .pipe(filterTest())
+    //   .pipe(gulpVue(api))
+    //   .pipe(
+    //     compileTS(void 0, {
+    //       allowJs: true,
+    //       declaration: false,
+    //       isolatedModules: true,
+    //     }),
+    //   )
+    //   .js.pipe(gulp.dest(TARGET_DIR));
+
     return new Promise((resolve, reject) => {
       merge2([
-        less,
-        tsResult.js
-          .pipe(convertLessImport2CssStream())
-          .pipe(gulp.dest(TARGET_DIR)),
-        tsResult.dts.pipe(gulp.dest(TARGET_DIR)),
-        tsJsResult.js
-          .pipe(convertLessImport2CssStream())
-          .pipe(gulp.dest(TARGET_DIR)),
-        assets,
-        vueResult,
+        less.pipe(gulp.dest(TARGET_DIR)),
+        assets.pipe(gulp.dest(TARGET_DIR)),
+        codeSourcePipeline.pipe(gulp.dest(TARGET_DIR)),
+        // tsResult.js
+        //   .pipe(convertLessImport2CssStream())
+        //   .pipe(gulp.dest(TARGET_DIR)),
+        // tsResult.dts.pipe(gulp.dest(TARGET_DIR)),
+        // tsJsResult.js
+        //   .pipe(convertLessImport2CssStream())
+        //   .pipe(gulp.dest(TARGET_DIR)),
+        // vueResult,
       ]).on('finish', () => {
         if (errors.length) {
           return reject(errors);
